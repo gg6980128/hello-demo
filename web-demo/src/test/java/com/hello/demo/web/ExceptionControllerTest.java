b2e2a1ab087e0080cf53b3231e62852fca1c80cb
GS2505-gg6980128 <gray6980128@gmail.com>
2025-06-04 17:42:40 +0800

    web全局异常处理测试

--- web-demo/src/test/java/com/hello/demo/web/ExceptionControllerTest.java
+++ web-demo/src/test/java/com/hello/demo/web/ExceptionControllerTest.java
@@ -1,15 +1,20 @@
 package com.hello.demo.web;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = ExceptionController.class)
-class ExceptionControllerWebMvcTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class ExceptionControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -17,7 +22,8 @@ class ExceptionControllerWebMvcTest {
     @Test
     void testException_shouldReturn500WithErrorMsg() throws Exception {
         mockMvc.perform(get("/exception"))
-                .andExpect(status().isInternalServerError())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("500"))
                 .andExpect(jsonPath("$.msg").value("测试异常"));
     }
 }
